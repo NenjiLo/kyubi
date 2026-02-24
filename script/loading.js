d85bebac0d8d73e6aad4c31651ab4cea421e73f7
NenjiLo <nenji.j.lo@gmail.com>
2026-02-24 18:15:37 +0000

    Tue, Feb 24, 2026, 10:15 AM -08:00

--- script/loading.js
+++ script/loading.js
@@ -3,6 +3,7 @@ bg = document.querySelector('.bg')
 
 load = 100
 
-loadText.innerText = '${load}%'
-loadText.styke.opacity = 1-load/100
-bg.style.filter = "blur(30px)"
+loadText.innerText = `${load}%`
+loadText.style.opacity = 1-load/100
+bg.style.filter = "blur(50px)"
+bg.style.filter = `blur((50-load}/2)px)`
