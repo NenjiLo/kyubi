9407190bca0d71a5c7ce333d51a91f82f4eabb0a
NenjiLo <nenji.j.lo@gmail.com>
2026-02-24 18:22:57 +0000

    Tue, Feb 24, 2026, 10:22 AM -08:00

--- script/loading.js
+++ script/loading.js
@@ -5,7 +5,13 @@ load = 0
 
 int = setInterval(blurring, 30)
 
-loadText.innerText = `${load}%`
-loadText.style.opacity = 1-load/100
-bg.style.filter = "blur(50px)"
-bg.style.filter = `blur(${50-load/2}px)`
+function blurring() {
+    load = load + 1
+    loadText.innerText = `${load}%`
+    loadText.style.opacity = 1-load/100
+    bg.style.filter = `blur(${50-load/2}px)`
+
+    if (load > 99) {
+        clearInterval(int)
+    }
+}
